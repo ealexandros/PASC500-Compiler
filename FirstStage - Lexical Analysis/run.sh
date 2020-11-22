ccae0df9e65d11fdf6909248843a6538895c2281
alex <alexandros441@yahoo.gr>
2020-11-22 21:02:24 +0200

    Added more Functions | Compiled it

---
+++ FirstStage - Lexical Analysis/run.sh
@@ -0,0 +1,3 @@
+flex pasc500.l
+gcc lex.yy.c
+./a.out $1
--- FirstStage - Lexical Analysis/tokens.h
+++ FirstStage - Lexical Analysis/tokens.h
@@ -28,7 +28,6 @@
 #define T_WHILE         21
 #define T_DO            22
 #define T_FOR           23
-
 #define T_ELSE          20
 #define T_THEN          19
 #define T_DOWNTO        24
@@ -39,3 +38,38 @@
 #define T_WITH          26
 #define T_READ          27
 #define T_WRITE         28
+
+// Operators
+#define T_RELOP         29
+#define T_ADDOP         30
+#define T_OROP          31
+#define T_MULDIVANDOP   32
+#define T_NOTOP         33
+#define T_INOP          34
+
+// Other Lexical units
+#define T_LPAREN        35
+#define T_RPAREN        36
+#define T_DOT           37
+#define T_COMMA         38
+#define T_EQU           39
+#define T_COLON         40
+#define T_LBRACK        41
+#define T_RBRACK        42
+#define T_ASSIGN        43
+#define T_DOTDOT        44
+#define T_SEMI          45
+
+// Identifiers
+#define T_IDENTIFIER    46
+#define T_ICONST        47
+#define T_RCONST        49
+
+// Logical Operators
+#define T_BCONST        48
+
+// Strings
+#define T_CCONST        50
+#define T_SCONST        51
+
+#define T_COMMENT       52
